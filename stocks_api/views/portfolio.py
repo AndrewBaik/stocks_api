32bb5b5971f61773b02ec8591ba2f92f7e5c0887
Andrew Baik <andrewbaik@outlook.com>
2018-08-23 16:00:39 -0700

    removed all f strings for aws deployment

--- stocks_api/views/portfolio.py
+++ stocks_api/views/portfolio.py
@@ -5,4 +5,4 @@ from pyramid.response import Response
 class Portfolio_api(APIViewSet):
     def retrieve(self, request, id=None):
         # http :6543/api/v1/company/{id}/
-        return Response(json={'message': f'Here is your portfolio {id}'}, status=200)
+        return Response(json={'message': 'Here is your portfolio'}, status=200)
--- stocks_api/views/stock.py
+++ stocks_api/views/stock.py
@@ -7,10 +7,10 @@ class Stock_api(APIViewSet):
         return Response(json={'message': 'List of all your stocks'}, status=200)
 
     def retrieve(self, request, id=None):
-        return Response(json={'message': f'Here is a stock {id}'}, status=200)
+        return Response(json={'message': 'Here is a stock'}, status=200)
 
     def create(self, request):
         return Response(json={'message': 'Created a new stock'}, status=201)
 
     def destroy(self, request, id=None):
-        return Response(json={'message': f'Removed a {id}'}, status=204)
+        return Response(json={'message': 'Removed a stock'}, status=204)
