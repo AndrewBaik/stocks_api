7db30f69261aae2fd1e7696f40c6470eda413c09
Andrew Baik <andrewbaik@outlook.com>
2018-08-27 23:00:04 -0700

    added doc strings and updated readme

--- stocks_api/views/company.py
+++ stocks_api/views/company.py
@@ -3,6 +3,10 @@ from pyramid.response import Response
 
 
 class Company_api(APIViewSet):
+    """ Base CRUD for Company
+    """
     def retrieve(self, request, id=None):
+        """ Get a selected Company
+        """
         # http :6543/api/v1/company/{id}/
         return Response(json={'message': 'Provided a single resource for company'}, status=200)
--- stocks_api/views/stock.py
+++ stocks_api/views/stock.py
@@ -3,14 +3,24 @@ from pyramid.response import Response
 
 
 class Stock_api(APIViewSet):
+    """ Base CRUD class for stock
+    """
     def list(self, request):
+        """ Get all stocks
+        """
         return Response(json={'message': 'List of all your stocks'}, status=200)
 
     def retrieve(self, request, id=None):
+        """ Get a selected stock
+        """
         return Response(json={'message': 'Here is a stock'}, status=200)
 
     def create(self, request):
+        """ Post a new stock
+        """
         return Response(json={'message': 'Created a new stock'}, status=201)
 
     def destroy(self, request, id=None):
+        """ Remove a selected stcok
+        """
         return Response(json={'message': 'Removed a stock'}, status=204)
